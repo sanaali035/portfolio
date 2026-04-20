07512c8e0f54258e970f9912aca72d8e2bd78017
sanaali035 <sanasali035@gmail.com>
2026-04-20 18:22:32 -0400

    added some experience

--- frontend/src/components/Experience.jsx
+++ frontend/src/components/Experience.jsx
@@ -2,7 +2,15 @@ function Experience() {
   return (
     <section id="experience">
       <h2>Experience</h2>
-      <p>Experience will go here.</p>
+      <h3> Teaching Assistant - Northeastern University</h3>
+      <h4> Khoury College of Computer Science • Boston, MA</h4>
+      <p>July 2024 - August 2024</p>
+      <ul>
+        <li>Led weekly lab sessions for 30+ students, reinforcing Python and problem-solving skills.</li>
+        <li>Supported students in office hours with debugging and course concepts.</li>
+        <li>Evaluated assignments, managed grading, and course coordination.</li>
+      </ul>
+      <h3>Software Developer & IT Analyst - Harvard Kennedy School</h3>
     </section>
   );
 }
