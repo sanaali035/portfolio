a50eba932f313e89d0642ff3544b80bdcd832a1a
sanaali035 <sanasali035@gmail.com>
2026-04-20 22:30:21 -0400

    added projects and experience

--- frontend/src/components/Experience.jsx
+++ frontend/src/components/Experience.jsx
@@ -2,6 +2,7 @@ function Experience() {
   return (
     <section id="experience">
       <h2>Experience</h2>
+
       <h3> Teaching Assistant - Northeastern University</h3>
       <h4> Khoury College of Computer Science • Boston, MA</h4>
       <p>July 2024 - August 2024</p>
@@ -10,7 +11,35 @@ function Experience() {
         <li>Supported students in office hours with debugging and course concepts.</li>
         <li>Evaluated assignments, managed grading, and course coordination.</li>
       </ul>
-      <h3>Software Developer & IT Analyst - Harvard Kennedy School</h3>
+
+      <h3>Software Developer & IT Analyst - Harvard Kennedy School</h3>                                            
+      <h4> Harvard Kennedy School • Cambridge, MA</h4>
+      <p>July 2023 - December 2023</p>
+      <ul>
+        <li>Automated ServiceNow ticket workflows using Power Automate, improving efficiency and resolution time.</li>
+        <li>Developed a centralized onboarding portal using Flask and SQL for new employees, streamlining the 
+          integration process and reducing manual IT workload.</li>
+        <li>Cleaned and structured training data for a training chatbot, improving accessbility of onboarding resources.</li>
+        <li>Resolved 488 technical issues across hardware, applications and operating systems.</li>
+      </ul>
+
+      <h3>Summer College Intern</h3>
+      <h4>New York City Department of Citywide Administrative Services (DCAS) • NYC </h4>
+      <p>July 2022 - August 2022</p>
+      <ul>
+        <li>Performed manual testing and test validation for the NYC Civil Service examination platform using Excel test scripts.</li>
+        <li>Mapped business requirem,ents and workflows through process flowcharts.</li>
+        <li>Participated in Agile meetings using Azure DevOps  to track requirements and project updates.</li>
+      </ul>
+
+      <h3>Girls Who Code</h3>
+      <h4>Johnson & Johnson • Raritan, NJ</h4>
+      <p>July 2019 - August 2019</p>
+      <ul>
+        <li>Developed an automated robot using Arduino & C++ to navigate through mazes and self problem solve through obstacles.</li>
+        <li>Designed a travel database platform with HTML, JS, Java & Node (JS) including a personalized survey & API linked 
+          to hundreds of traveling resources & popular tourist locations.</li>
+      </ul>
     </section>
   );
 }
--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -2,7 +2,26 @@ function Projects() {
   return (
     <section id="projects">
       <h2>Projects</h2>
-      <p>Projects will go here.</p>
+      
+      <h3>Strategy.town</h3>
+      <h4>May 2025- June 2025</h4>
+      <p>Strategy.town is a full-stack web application developed using React (Typescript) and Node.js, focused on interactive
+        gameplay and user-driven features.
+
+        The system include3s RESTful APIs to manage game state, user data, and statistics, along with dynamic frontend 
+        components that support real-time updates and seamless navigation. Application logic and API functionality were
+        tested using Vitest and Postman to ensure reliability and alignment with defined user requirements.
+      </p>
+
+      <h3>AI Hoops!</h3>
+      <h4> July 2024 - August 2024</h4>
+      <p> AI Hoops is a reinforcement learning project where a Double Deep Q-Network (DDQN) was developed using PyTorch
+        to simulate intelligent gameplay in a Atari basketball environment.
+
+        The model was optimized by using low-dimensional RAM inputs insteade of high-dimensional image data, reducing
+        training complexity while maintaining performance. Key reinforcement learning techniques such as experience replay
+        and epsilon-greedy policies were applied to improve convergence and enable adaptive decision-making.
+      </p>
     </section>
   );
 }
